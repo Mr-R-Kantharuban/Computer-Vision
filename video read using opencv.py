b5671d75394e1c89d454282df4d83b71eb0b3d9e
R.Kantharuban <101087213+Mr-R-Kantharuban@users.noreply.github.com>
2022-03-08 09:22:06 +0530

    Add files via upload

---
+++ video read using opencv.py
@@ -0,0 +1,13 @@
+import cv2
+
+framewidth=640
+frameheight=360
+
+cap=cv2.VideoCapture("resources/sample.mp4")
+
+while True:
+    success,img=cap.read()
+    cv2.imshow("video",img)
+
+    if cv2.waitKey(0) & 0xFF == ord('q'):
+        break
